01cbef5dd326494cb70fca0e85372e4960e80d3f
benwu <wubinben@icloud.com>
2019-08-04 17:59:02 +0800

    back to baseline

--- src/main/java/kata/taminglegacycode/tlc6/BankingController.java
+++ src/main/java/kata/taminglegacycode/tlc6/BankingController.java
@@ -1,8 +1,8 @@
 package kata.taminglegacycode.tlc6;
 
 public class BankingController {
-    public void updateAccountBalance(int sum, String id, SMService smService, BankingService bankingService) {
-        bankingService.updateAccountBalance_instance(id, sum);
+    public void updateAccountBalance(int sum, String id, SMService smService) {
+        BankingService.updateAccountBalance(id, sum);
         if (sum > 1000) {
             smService.send("your balance is " + sum);
         }
