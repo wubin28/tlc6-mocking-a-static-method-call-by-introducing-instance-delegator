e089bc2965f0ffeb263ffb433f682413f566c500
benwu <wubinben@icloud.com>
2019-08-04 17:56:00 +0800

    make test should_send_short_message_if_sum_is_big_enough() green

--- src/main/java/kata/taminglegacycode/tlc6/BankingService.java
+++ src/main/java/kata/taminglegacycode/tlc6/BankingService.java
@@ -2,5 +2,10 @@ package kata.taminglegacycode.tlc6;
 
 public class BankingService {
     public static void updateAccountBalance(String id, int sum) {
+        // a lot of logic we don't want to run in our tests.
+    }
+
+    public void updateAccountBalance_instance(String id, int sum) {
+        updateAccountBalance(id, sum);
     }
 }
