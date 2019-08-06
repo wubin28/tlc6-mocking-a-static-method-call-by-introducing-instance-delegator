54383c7a7d518a021119060c13d20c0aab9b6e14
benwu <wubinben@icloud.com>
2019-08-06 16:28:15 +0800

    make SMService.send() unsupported

--- src/main/java/kata/taminglegacycode/tlc6/SMService.java
+++ src/main/java/kata/taminglegacycode/tlc6/SMService.java
@@ -2,6 +2,6 @@ package kata.taminglegacycode.tlc6;
 
 public class SMService {
     public void send(String s) {
-
+        throw new UnsupportedOperationException("SMService.send() not supported yet.");
     }
 }
